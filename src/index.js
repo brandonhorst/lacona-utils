8036a550a0cd11073d259b6b97c748bd14c7efff
Brandon Horst <brandonhorst@gmail.com>
2016-05-05 15:09:28 -0400

    Group by qualifiers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import _ from 'lodash'
 
+function groupPlaceholders (output) {
+  return _.chain(output.words)
+    .filter(item => item.placeholder || item.text)
+    .map(item => item.placeholder ? '\uFFFC' : `\uFFF9${item.text}\uFFFA${item.argument}\uFFFB`)
+    .join('')
+    .value()
+}
+
 function groupPlaceholdersAndQualifiers (output) {
   return _.chain(output.words)
     .filter(item => item.placeholder || item.text)
@@ -18,7 +26,7 @@ function mapPlaceholderGroups (outputGroup) {
         .value()
     })
     .thru(descriptorLists => _.zip(...descriptorLists))
-    .map(x => _.unique(x))
+    .map(x => _.uniq(x))
     .map(x => _.filter(x))
     .value()
 
@@ -35,10 +43,34 @@ function mapPlaceholderGroups (outputGroup) {
   return output
 }
 
+function flatMapQualifiers (outputGroup) {
+  // PERF
+  // if there is only one item, then we don't need qualifiers
+  if (outputGroup.length === 1) {
+    return [_.assign({}, outputGroup[0], {qualifiers: []})]
+  }
+
+  const allQualifiers = _.map(outputGroup, 'qualifiers')
+  const commonQualifiers = _.intersection(...allQualifiers)
+
+  // PERF
+  // if there are no commonQualifiers, no need to do all this work
+  if (commonQualifiers.length) {
+    return _.map(outputGroup, output => {
+      const qualifiers = _.difference(output.qualifiers, commonQualifiers)
+      return _.assign({}, output, {qualifiers})
+    })
+  } else {
+    return outputGroup
+  }
+}
+
 export function combinePlaceholders (outputs, limit = 100) {
   return _.chain(outputs)
     .groupBy(groupPlaceholdersAndQualifiers)
     .map(mapPlaceholderGroups)
+    .groupBy(groupPlaceholders)
+    .flatMap(flatMapQualifiers)
     .sortBy(option => -option.score)
     .take(limit)
     .value()
